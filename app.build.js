34a80a144aec91f4da035b656be0c080a1628c6b
nkasprak <nkasprak@gmail.com>
2015-05-13 16:23:12 -0400

    Set up r.js concat package
    and some more tiny tweaks

---
+++ app.build.js
@@ -0,0 +1,11 @@
+({
+    appDir: ".",
+    baseUrl: "app",
+    dir: "higher_ed_2015_build",
+    mainConfigFile: 'app/main.js',
+    modules: [
+        {
+            name: "main"
+        }
+    ]
+})
